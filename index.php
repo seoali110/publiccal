c10dccce3082bdee95df193b45ba32b425c8431f
seoali110 <145331742+seoali110@users.noreply.github.com>
2023-09-18 14:01:01 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculator Website</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 0;
+        }
+        header {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 10px 0;
+        }
+        nav {
+            background-color: #444;
+            text-align: center;
+            padding: 10px 0;
+        }
+        nav ul {
+            list-style: none;
+            padding: 0;
+        }
+        nav ul li {
+            display: inline;
+            margin-right: 20px;
+        }
+        .container {
+            max-width: 800px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+        }
+        .categories {
+            background-color: #eee;
+            padding: 10px;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Calculator Website</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="#category1">Mortgage Calculator</a></li>
+            <li><a href="#category2">Loan Calculators</a></li>
+            <li><a href="#category3">All in one Calculator</a></li>
+        </ul>
+    </nav>
+    <div class="container">
+        <section class="categories">
+            <h2>Our Collection</h2>
+            <ul>
+                <li><a href="#category1">Mortgage Calculator</a></li>
+                <li><a href="#category2">Loan Calculators</a></li>
+                <li><a href="#category3">All in One Calculator</a></li>
+            </ul>
+        </section>
+        <section id="category1">
+            <h2>Mortgage Calculator</h2>
+            <!-- Add Category 1 Calculator PHP code here -->
+            <p>Mortgage calculators are essential tools for anyone considering buying a home or refinancing an existing mortgage. These calculators provide valuable insights into the financial aspects of home ownership. By entering details like the loan amount, interest rate, loan term, and down payment, you can determine your monthly mortgage payments, the total interest you'll pay over the life of the loan, and the impact of different scenarios on your budget.
+
+                Mortgage calculators empower potential homeowners to make informed decisions, allowing them to find a mortgage that suits their financial situation and goals. Whether you're a first-time buyer, a seasoned homeowner, or exploring refinancing options, mortgage calculators are your trusted companions on the path to responsible home financing.</p>
+        </section>
+        <section id="category2">
+            <h2>Loan Calculators</h2>
+            <!-- Add Category 2 Calculator PHP code here -->
+            <p>
+                Loan calculators are invaluable tools that help individuals and businesses make informed financial decisions. Whether you're planning to take out a mortgage, a personal loan, or a business loan, these calculators allow you to estimate monthly payments, interest costs, and the overall affordability of your loan. By inputting key variables such as loan amount, interest rate, and loan term, you can quickly assess different scenarios and tailor your borrowing strategy to meet your financial goals. Loan calculators empower borrowers with the knowledge they need to manage their finances wisely, ensuring that loans are a manageable and sustainable part of their financial journey.
+            </p>
+        </section>
+        <section id="category3">
+            <h2>All in one Calculators</h2>
+            <!-- Add Category 3 Calculator PHP code here -->
+            <p>All-in-one calculators <a href="https://publiccalculators.com/">Public Calculators</a> versatile tools designed to handle a wide range of mathematical and financial calculations in a single, user-friendly interface. These calculators offer convenience and efficiency by combining various functions and features into one comprehensive application. Users can perform basic arithmetic operations, complex mathematical calculations, convert units, and even tackle financial tasks such as loan calculations, mortgage amortization, and currency conversions—all within the same tool.
+
+                The appeal of all-in-one calculators lies in their flexibility and time-saving capabilities. They are particularly valuable for students, professionals, and individuals from diverse fields who need quick access to mathematical and financial tools without the hassle of switching between multiple apps or websites. With user-friendly interfaces and the ability to customize settings for specific needs, all-in-one calculators are indispensable companions for anyone who requires accurate calculations and conversions on a daily basis.
+                
+                These versatile calculators not only simplify mathematical tasks but also contribute to greater productivity and accuracy in a wide range of professions, from engineering and finance to education and everyday life. Whether you need to solve equations, calculate percentages, or convert currencies, all-in-one calculators are the Swiss Army knives of the digital age, making complex computations accessible to all.</p>
+            <!-- Add Category 3 Calculator PHP code here -->
+        </section>
+    </div>
+</body>
+</html>
